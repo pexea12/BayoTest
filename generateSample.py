c301601f3e8ae2e09e46edd3bdcbf7a116a3a144
Dzung Nguyen <dzungnguyenvn@outlook.com>
2016-09-01 02:19:52 +0700

    Complete generate data files

---
+++ generateSample.py
@@ -0,0 +1,31 @@
+import random
+import numpy as np
+
+def generateData(n=int(1e6), seed=None):
+	'''
+	Generate n random signed 32-bit integers (from -2^31 to 2^31 - 1)
+	Params:
+		n: number of random integers (default = 1e6)
+		seed: the seed of random (default = None (use system time))
+	Return:
+		A list which contains n random integers
+	'''
+	# Set the seed of random
+	np.random.seed(seed)
+	
+	return np.random.randint(-2**31, 2**31 - 1, size=n)
+
+	
+def writeDataToFile(filename='input.txt', n=int(1e6), seed=None):
+	'''	
+	Write n random signed 32-bit integers to file filename
+	'''
+	array = generateData(n=n, seed=seed)
+	
+	np.savetxt(filename, array, delimiter='\n', fmt='%d')
+	
+	
+	
+	
+	
+	
